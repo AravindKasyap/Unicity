1dbd40718bb72a51cab421fef80e6aafa65dd723
MacBookTesting <applemacbookpro@projects.sritads.com>
2018-06-06 14:17:11 +0530

    done

---
+++ app/src/main/java/com/example/applemacbookpro/unicity/settings.java
@@ -0,0 +1,20 @@
+package com.example.applemacbookpro.unicity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class settings extends Fragment {
+    @Override
+
+    public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle savedInstanceState){
+        View View= inflater.inflate(R.layout.settings,container,false);
+        return View;
+    }
+
+
+
+}
